c00331dc1003385d6a7fec443fee3d22c35f75cd
seba0205 <83637951+seba0205@users.noreply.github.com>
2021-08-15 12:13:20 +1200

    popup now works

--- main.js
+++ main.js
@@ -1,50 +1,102 @@
-let money = 0;
-let goals = [];
+loadGoals();
+refresh();
 
-class Goal {
-    name
-    current
-    total
-
-    constructor(name, total) {
-        this.name = name;
-        this.total = total;
-        this.current = 0;
-    }
-
-    toString() {
-        return `${this.name}, ${this.current}, ${this.total}`
+function refresh() {
+    let UI_goalsList = document.getElementById("UI_goalsList");
+    UI_goalsList.innerHTML = "";
+    let textArea = document.getElementById("textArea");
+    for (name in goals) {
+        UI_goalsList.appendChild(makeGoalElement(name));
     }
 }
 
+function makeGoalElement(name) {
+    let superElement = document.createElement("div");
+    superElement.className = "col";
+    let element = document.createElement("div");
+    superElement.appendChild(element);
+    element.className = "card";
+    let linkElement = document.createElement("a");
+    linkElement.className = "goalLink";
+    linkElement.setAttribute("href", "./goalDetailPage.html?name=" + name);
+    linkElement.innerText = goals[name].toString();
+    element.appendChild(linkElement);
+    let button = document.createElement("button");
+    button.className = "btn btn-primary"
+    button.innerText = "+";
+    button.onclick = () => {
+        incrementFromName(name)
 
-function getGoalString() {
-    let output = "";
-    for (goal of goals) {
-        output += goal.toString() + "\n";
+        //refresh the page
+        refresh();
     }
-    return output;
+    element.append(button);
+
+    return superElement;
 }
 
-function buttonOnClick() {
+function addGoal() {
     let nameInput = document.getElementById("nameInput");
     let totalInput = document.getElementById("totalInput");
-    let textArea = document.getElementById("textArea");
 
     let name = nameInput.value;
     let total = totalInput.value;
-
     let number = parseFloat(total)
+
+    // If the name is empty
+    if (name.trim().length === 0) {
+        return;
+    }
+
     if (isNaN(number)) {
         window.alert("Error, not a number. Please try again.")
         return;
     }
 
-    let goal = new Goal(name, number);
-    goals.push(goal);
-    textArea.innerText = getGoalString();
+    mutateData(() => goals[name] = new Goal(name, number), refresh);
 
     // clear the text inputs
     nameInput.value = "";
     totalInput.value = "";
+
+}
+
+function incrementFromName(name) {
+    if (!goals[name]) {
+        return;
+    }
+
+    mutateData(() => goals[name].increment(), refresh);
+}
+
+function incrementCurrent() {
+    let editNameInput = document.getElementById("editNameInput");
+    let incrementAmountInput = document.getElementById("incrementAmount");
+
+    let name = editNameInput.value;
+    let amountToAdd = parseFloat(incrementAmountInput.value);
+
+    // If the name is empty
+    if (name.trim().length === 0) {
+        return;
+    }
+
+    // If there is no goal with this name
+    if (!goals[name]) {
+        window.alert("Error, that goal name does not exist. Please try again.")
+        return;
+    }
+
+    // If not a number
+    if (isNaN(amountToAdd)) {
+        window.alert("Error, not a number. Please try again.")
+        return;
+    }
+
+    mutateData(() => goals[name].incrementByAmount(amountToAdd), refresh);
+
+    // clear the text inputs
+    editNameInput.value = "";
+    incrementAmountInput.value = "";
+
 }
